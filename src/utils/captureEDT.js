63fce36635854d97ff896428fcb982c0e2cac04e
Maner <antonin.huaut+github@gmail.com>
2020-05-25 15:40:47 +0200

    change lib

--- src/utils/captureEDT.js
+++ src/utils/captureEDT.js
@@ -1,32 +1,40 @@
-const playwright = require('playwright');
+const puppeteer = require('puppeteer');
 const sharp = require('sharp');
 const config = require('../../config.json');
 const fs = require('fs').promises;
 
 exports.takeScreenshot = async function (edtId, folder, fileName) {
-    const browser = await playwright['firefox'].launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.setViewportSize({
-        width: config.img.viewport.width,
-        height: config.img.viewport.height
-    });
-    await page.goto(`${config.url.front}/edt/${edtId}`);
     try {
-        await page.waitForSelector('.v-calendar', {
-            visible: true
+        const width = config.img.viewport.width;
+        const height = config.img.viewport.height;
+
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox', `--window-size=${width},${height}`]
         });
-    } catch (e) {}
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: width,
+            height: height
+        });
+        await page.goto(`${config.url.front}/edt/${edtId}`);
+        try {
+            await page.waitForSelector('.v-calendar', {
+                visible: true
+            });
+        } catch (e) {}
 
-    try {
-        await fs.mkdir(folder);
-    } catch {}
+        try {
+            await fs.mkdir(folder);
+        } catch {}
 
-    await page.screenshot({
-        path: `${folder}in-${fileName}`
-    });
+        await page.screenshot({
+            path: `${folder}in-${fileName}`
+        });
 
-    await browser.close();
+        await browser.close();
+    } catch (ex) {
+        console.error(ex)
+    }
 }
 
 exports.resizeScreenshot = async function (folder, fileName) {
